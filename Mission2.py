d360c67c6d2dec1e229bba26753b3b32bc19555b
osj0201 <osj020201@gmail.com>
2024-10-11 21:06:13 +0900

    Mission2

--- Mission2.py
+++ Mission2.py
@@ -1,8 +1,12 @@
-# class 
+class person:
+    def __init__(self, name, gender, age):
+        self.name = name
+        self.gender = gender
+        self.age = age
+        return 
 
-# person -> name, gender, age 멤버 변수 설정
-# name(문자) gender(문자) age(정수) 
-# 생성자 __init__
+
+
 # display() > name gender age
 
 # person 객체를 만들고 -> display person 데이터 값을 입력을 해서 출력
