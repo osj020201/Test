3a77b2a97a79c43349ea054c8a4512b4a0e3b2d5
osj0201 <osj020201@gmail.com>
2024-10-11 17:01:09 +0900

    Mission2

---
+++ Mission2.py
@@ -0,0 +1,8 @@
+# class 
+
+# person -> name, gender, age 멤버 변수 설정
+# name(문자) gender(문자) age(정수) 
+# 생성자 __init__
+# display() > name gender age
+
+# person 객체를 만들고 -> display person 데이터 값을 입력을 해서 출력
