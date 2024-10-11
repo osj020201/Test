15ea5d9828d757ef22307797c704eb8cab1f92b9
osj0201 <osj020201@gmail.com>
2024-10-11 16:17:41 +0900

    Mission1

---
+++ Mission1.py
@@ -0,0 +1,17 @@
+import random
+
+number = random.randint(1, 10)
+
+print("컴퓨터가 생각했을 만한 숫자를 입력하세요!: ")
+while True:
+    if i == number:
+
+        
+
+# 처음에는 조건문 ->  GPT한테 정의같은 것만 물어보다가
+# While, for in <- 반복문 필요할거같아서 생각남
+
+# 숫자 정하기 
+# input("예상 숫자: ")
+# 너무큼 다시입력 -> while , comtinue
+# 맞으면 정답 print("정답 입니다.")
--- test.py
+++
@@ -1,5 +0,0 @@
-import random
-
-random_lender = ("1 에서 100 사이의 숫자 하나를 정했습니다.")
-("이 숫자는 무엇일까요?")
-print(random_lender)
